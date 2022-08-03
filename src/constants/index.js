237342c63d0ee49a90c4fd0f5e8a4854b4d4fbf1
Christian Sanchez <christian.s.molina@gmail.com>
2022-08-03 03:02:31 -0500

    Constants created

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,14 @@
 export const ROWS = 20
+
 export const COLS = 20
+
+export const POSITIONS = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+];
