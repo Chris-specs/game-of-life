aece44ecf2672ae200ba4a69325473141dce6bea
Christian Sanchez <christian.s.molina@gmail.com>
2022-08-03 03:03:09 -0500

    Adding Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+// Components
+import Grid from './components/Grid';
+// Styles
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Game of life</h1>
-    </div>
-  );
+    return (
+        <main className='flex justify-center'>
+            <section className='flex flex-col items-center'>
+                <h1 className='text-4xl font-semibold py-4'>Game of life</h1>
+                <Grid />
+            </section>
+        </main>
+    );
 }
 
 export default App;
