dc2f40692985bf864c8dc9ea3bde6f1baabee1f7
Christian Sanchez <christian.s.molina@gmail.com>
2022-08-05 14:05:28 -0500

    Clean cells function added

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,26 +7,32 @@ export const randomCells = () => (
     ))
 )
 
+export const cleanCells = () => (
+    Array(ROWS).fill().map(( row ) => (
+        Array(COLS).fill(0)
+    ))
+)
+
 export const simulation = ( grid ) => (
     Array(ROWS).fill().map(( row, iR ) => (
         Array(COLS).fill().map(( col, iC ) => {
             let aliveNeighbors = 0
-
+            
             POSITIONS.forEach(([ x, y ]) => {
                 const newX = iR + x
                 const newY = iC + y
-
+                
                 if (newX >= 0 && newX < ROWS && newY >= 0 && newY < COLS) {
                     aliveNeighbors += grid[newX][newY];
                 }
             })
 
             if (aliveNeighbors < 2 || aliveNeighbors > 3) {
-                grid[iR][iC] = 0;
-            } else if (grid[iR][iC] === 0 && aliveNeighbors === 3) {
-                grid[iR][iC] = 1;
+                return 0;
+            } else if (aliveNeighbors === 3) {
+                return 1;
             }
             return grid[iR][iC]
-        } )
+        })
     ))
 )
