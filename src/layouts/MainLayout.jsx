d2c47ce9ac6317fb8fb5e8547040f175056212cb
Christian Sanchez <christian.s.molina@gmail.com>
2022-08-05 22:25:20 -0500

    Unnecessary deps removed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const MainLayout = ({ children }) => {
     return (
         <>
             <header>
-                <nav className='flex justify-center lg:justify-between px-10 py-2'>
+                <nav className='flex justify-center lg:justify-start px-10 py-2'>
                     <div className='flex items-center'>
                         <img className='w-16 h-16' src='/assets/img/glider.png' alt="Glider" />
                         <p className='text-2xl font-semibold py-4'>Game of life</p>
