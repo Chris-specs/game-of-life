ec3f81366b9338d1021652c5bbd0fc7ee0ccadd8
Christian Sanchez <christian.s.molina@gmail.com>
2022-08-05 21:48:37 -0500

    ROW_CENTER and COL_CENTER constants added

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,10 @@ export const ROWS = 20
 
 export const COLS = 20
 
+export const ROW_CENTER = parseInt((ROWS / 2) - 1)
+
+export const COL_CENTER = parseInt((COLS / 2) - 1)
+
 export const POSITIONS = [
     [0, 1],
     [0, -1],
