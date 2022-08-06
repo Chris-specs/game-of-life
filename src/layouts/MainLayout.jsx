1b73783f91f398628bbb61e458c045bf1e1201ea
Christian Sanchez <christian.s.molina@gmail.com>
2022-08-05 22:13:23 -0500

    Main layout added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MainLayout = ({ children }) => {
+    return (
+        <>
+            <header>
+                <nav className='flex justify-center lg:justify-between px-10 py-2'>
+                    <div className='flex items-center'>
+                        <img className='w-16 h-16' src='/assets/img/glider.png' alt="Glider" />
+                        <p className='text-2xl font-semibold py-4'>Game of life</p>
+                    </div>
+                </nav>
+            </header>
+            <main className='flex justify-center pt-20'>
+                { children }
+            </main>
+        </>
+    )
+}
+
+export default MainLayout
